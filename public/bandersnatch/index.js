c0b6b99a9f8b201cb16a0dc54fd6d22957e8c336
hppod <hppodgornik@gmail.com>
2020-10-29 00:36:51 +0000

    Importando network no index e instanciando ele

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -6,8 +6,10 @@ async function main() {
     const manifestJSON = await (await fetch(MANIFEST_URL)).json()
     const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost
     const videoComponent = new VideoComponent()
+    const network = new Network({ host })
     const videoPlayer = new VideoMediaPlayer({
-        manifestJSON
+        manifestJSON,
+        network
     })
 
     videoPlayer.initializeCodec()
